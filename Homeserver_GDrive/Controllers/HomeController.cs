c725722807b868070b91fea9d0a95cffb756e8c1
Colin Kern <ckern101@gmail.com>
2025-03-13 18:05:11 -0700

    Add information for home view

--- Homeserver_GDrive/Controllers/HomeController.cs
+++ Homeserver_GDrive/Controllers/HomeController.cs
@@ -1,12 +1,17 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Homeserver_GDrive.Models;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
 
 namespace Homeserver_GDrive.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    static string[] Scopes = { DriveService.Scope.Drive };
+    static string ApplicationName = "penthouse-gdrive";
 
     public HomeController(ILogger<HomeController> logger)
     {
@@ -15,7 +20,23 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        GoogleCredential credential;
+        using (FileStream stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+        {
+            credential = GoogleCredential.FromStream(stream).CreateScoped(Scopes);
+        }
+        // Create Drive API service.
+        DriveService driveService = new DriveService(new BaseClientService.Initializer
+        {
+            HttpClientInitializer = credential,
+            ApplicationName = ApplicationName
+        });
+
+        AboutResource.GetRequest aboutUser = driveService.About.Get();
+        aboutUser.Fields = "kind,user,storageQuota";
+        Google.Apis.Drive.v3.Data.About userInfo = aboutUser.Execute();
+
+        return View(new HomeViewModel() { ServiceAccountName = userInfo.User.DisplayName });
     }
 
     public IActionResult Privacy()
---
+++ Homeserver_GDrive/Models/HomeViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Homeserver_GDrive.Models
+{
+    public class HomeViewModel
+    {
+        public string ServiceAccountName { get; set; } = "";
+    }
+}
