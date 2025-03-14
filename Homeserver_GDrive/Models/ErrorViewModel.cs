a81afa2338790758e572fce487693f7a08b11264
Colin Kern <ckern101@gmail.com>
2025-03-13 17:57:00 -0700

    Add project files.

---
+++ Homeserver_GDrive/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Homeserver_GDrive.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
